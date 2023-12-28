e0cbb427b2e224be31f83b8071e321dd1546ade2
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2023-12-28 14:35:16 -0500

    Cambiando one dayk

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { Pruebas } from './Pruebas'
 import { PruebasDiPruebas } from './PruebasDiPruebas'
 
 
-
+// practicas de un dia 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <PrimerComponente titulo='Enviando desde el padre' sumando={3}/> */}
