6520df6e8b222a635228bd82cc8b388d3a59e13c
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2024-01-02 19:34:23 -0500

    oneemoth test

--- src/Contador.jsx
+++ src/Contador.jsx
@@ -1,24 +1,18 @@
 import { useState } from "react";
 
-export const Contador = ({ value }) => {
+import React from 'react'
 
-  const[contador,setContador]=useState(value)
+const Botton =()=>{
+  return(
+    <button>Hola soy un botom</button>
+  )
+}
 
-  
-  const cliquendo = () => {
-    setContador(contador+5);
-  };
-
+export const Contador = () => {
   return (
     <>
-      <h1 className=" text-3xl font-bold ">Esto es un contador</h1>
-      <p className=" my-4">{contador}</p>
-      <button
-        className=" bg-gray-800 rounded-lg text-yellow-300 p-4"
-        onClick={cliquendo}
-      >
-        Soy un Boton
-      </button>
+        <Botton></Botton>
     </>
-  );
-};
+
+  )
+}
