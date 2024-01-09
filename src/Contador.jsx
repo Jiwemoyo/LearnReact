5335aaad2621bfbdfab898a1d6fa1a2c734363db
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2024-01-08 20:16:02 -0500

    Eventos M

--- src/Contador.jsx
+++ src/Contador.jsx
@@ -2,16 +2,17 @@ import { useState } from "react";
 
 import React from 'react'
 
-const Botton =()=>{
-  return(
-    <button>Hola soy un botom</button>
-  )
-}
 
-export const Contador = () => {
+export const Contador = ({value}) => {
+
+  function aumentando() {
+    let aumento = value++
+    console.log(aumento)
+  }
   return (
     <>
-        <Botton></Botton>
+        <h1 className=" font-bold text-5xl py-8">0</h1>
+        <button className=" bg-slate-400 rounded-xl p-7 text-white font-bold" onClick={aumentando}>Hola soy un botom</button>
     </>
 
   )
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Contador } from './Contador'
 // Se realizan cambios cada mes
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Contador></Contador>
+    <Contador value={0}></Contador>
   </React.StrictMode>
 )
 
