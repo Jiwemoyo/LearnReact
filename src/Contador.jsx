08c2046b96b20c11693e41b46955944c3812c00a
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2024-01-10 14:11:15 -0500

    hoock use state

--- src/Contador.jsx
+++ src/Contador.jsx
@@ -5,13 +5,14 @@ import React from 'react'
 
 export const Contador = ({value}) => {
 
+  const [cont, setcont] = useState(value)
+
   function aumentando() {
-    let aumento = value++
-    console.log(aumento)
+    setcont(cont+1)
   }
   return (
     <>
-        <h1 className=" font-bold text-5xl py-8">0</h1>
+        <h1 className=" font-bold text-5xl py-8">{cont}</h1>
         <button className=" bg-slate-400 rounded-xl p-7 text-white font-bold" onClick={aumentando}>Hola soy un botom</button>
     </>
 
