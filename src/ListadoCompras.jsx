4e8ab21eb9d6ac57e6e6eb4f967971b7ad1435e1
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2024-01-14 10:28:39 -0500

    map  M

--- src/ListadoCompras.jsx
+++ src/ListadoCompras.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { useState } from "react";
 
 const Producto = ({nombre,comprado}) => {
-  return <li>{nombre}: {comprado?'👌':'❌'}</li>;
+  return <li className=" font-bold text-3xl py-5">{nombre}: {comprado?'👌':'❌'}</li>;
 };
 
 const ListadoCompras = () => {
+  let arregloTienda =[
+    {nombre:'Pan',comprado:true},
+    {nombre:'Azucar',comprado:true},
+    {nombre:'Amargo',comprado:true},
+    {nombre:'Limon',comprado:true},
+    {nombre:'Sal',comprado:false}
+  ]
+
+  const [arregloT, setarregloT] = useState(arregloTienda)
+
   return (
     <>
       <h1 className=" text-4xl font-bold text-red-400">Lista de compras</h1>
       <ol>
-        <Producto nombre={'paN'} comprado={false}></Producto>
-        <Producto nombre={'Azucar'} comprado={true}></Producto>
-        <Producto nombre={'chocolate'} comprado={true}></Producto>
+        {arregloT.map(product => <Producto key={product.nombre} nombre={product.nombre} comprado={product.comprado}></Producto>)}
       </ol>
     </>
   );
